ad947b020bef9206376fae3353dfe4af16f32fac
sogatest <45475267+sogatest@users.noreply.github.com>
2018-11-30 11:54:07 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?
-echo 'php ƒeƒXƒg<br>';
+echo 'php ãƒ†ã‚¹ãƒˆ<br>';
 $dbinfo = parse_url(getenv('DATABASE_URL'));
 $dsn = 'pgsql:host=' . $dbinfo['host'] . ';dbname=' . substr($dbinfo['path'], 1);
 $pdo = new PDO($dsn, $dbinfo['user'], $dbinfo['pass']);
